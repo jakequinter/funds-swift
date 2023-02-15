4522721417f8a1957fd5998a178d97ccd1c478ca
Jake Quinter <hello@jakequinter.io>
2023-02-15 18:20:45 -0500

    get history view working again

--- Funds/ViewModels/HistoryViewModel.swift
+++ Funds/ViewModels/HistoryViewModel.swift
@@ -10,7 +10,38 @@ import FirebaseFirestoreSwift
 import Foundation
 
 @MainActor class HistoryViewModel: ObservableObject {
+    @Published var budgets = [Budget]()
+    @Published var years = [Int]()
     
     private var db = Firestore.firestore()
     
+    func fetchHistory() {
+        guard let currentUser = Auth.auth().currentUser else { return }
+        
+        db.collection("budgets").whereField("userId", isEqualTo: currentUser.uid).addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No budgets in HistoryViewModel")
+                return
+            }
+            
+            self.budgets = documents.compactMap { queryDocumentSnapshot -> Budget? in
+                return try? queryDocumentSnapshot.data(as: Budget.self)
+            }
+            
+            self.findUniqueYears()
+        }
+    }
+    
+    func findUniqueYears() {
+        var hasOccuredYears = [Int]()
+        
+        for budget in self.budgets {
+            if hasOccuredYears.contains(budget.year) {
+                continue
+            } else {
+                self.years.append(budget.year)
+                hasOccuredYears.append(budget.year)
+            }
+        }
+    }
 }
--- Funds/Views/HistoryView.swift
+++ Funds/Views/HistoryView.swift
@@ -12,12 +12,20 @@ struct HistoryView: View {
     
     var body: some View {
         NavigationStack {
-            Text("hi")
+            List(viewModel.years, id: \.self) { year in
+                Section(String(year)) {
+                    ForEach(viewModel.budgets.filter { $0.year == year }) { budget in
+                        Text(String(Calendar.current.monthSymbols[budget.month - 1]))
+                    }
+                }
+            }
+            .navigationTitle("History")
         }
-        .navigationTitle("History")
+        .onAppear {
+            self.viewModel.fetchHistory()
+        }
     }
 }
-
 
 struct HistoryView_Previews: PreviewProvider {
     static var previews: some View {
